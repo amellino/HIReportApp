248c14b70bc7e25d4e182d7755001bf09a8e847d
Andy Mellino <amellino@gmail.com>
2016-01-12 03:58:54 +0000

    Initial application commit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var App = React.createClass({
+    render() {
+        return <div><h1>Hello Home Inspector</h1><p>dis da app 4 u</p></div>;
+    }
+})
+
+React.render(<App/>, document.getElementById('example'));
